f552969c4a10b9e69a09d297c886cf6f7b23c040
arahmaninov <arahmaninov@yandex.ru>
2023-11-02 18:32:20 +0300

    add Stripe api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shop_sample_go
 
 go 1.21.1
+
+require github.com/stripe/stripe-go/v76 v76.2.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/paymentintent"
+	//"github.com/stripe/stripe-go/v76/customer"
 	"log"
 	"net/http"
 )
@@ -17,13 +21,66 @@ func main() {
 }
 
 func handleAbout(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ENDPOINT CALLED: handleAbout")
 	
 	response := []byte("Test server built in Go")
-	w.Write(response)
+	_, err := w.Write(response)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ENDPOINT CALLED: handleCreatePaymentIntent")
+
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+
+	var request struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName string `json:"last_name"`
+		Address1 string `json:"address_1"`
+		Address2 string `json:"address_2"`
+		City string `json:"city"`
+		State string `json:"state"`
+		Zip string `json:"zip"`
+		Country string `json:"country"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	// Stripe api 
+	params := &stripe.PaymentIntentParams{
+		Amount: stripe.Int64(calculateOrderAmount(request.ProductId)),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+
+	paymentIntent, err := paymentintent.New(params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Println(paymentIntent.ClientSecret)
 }
 
+func calculateOrderAmount(productId string) int64 {
+	switch productId {
+	case "Forever Pants":
+		return 26000
+	case "ForeverShirt":
+		return 15500
+	case "Forever Shorts":
+		return 30000
+	}
+
+	return 0
+}
